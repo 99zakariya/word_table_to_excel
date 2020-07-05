2ea8d3129ff66fdf28b65e71b1a03b47d47d9604
zakariya <34689684+99zakariya@users.noreply.github.com>
2020-07-06 04:21:52 +0530

    Add files via upload

--- word_table_to_excel.py
+++ word_table_to_excel.py
@@ -4,6 +4,7 @@ Created on Sat Jul  4 02:16:18 2020
 
 @author: zak
 """
+import time 
 import os
 import pandas as pd
 from docx.api import Document
@@ -21,79 +22,87 @@ data=[]
 path=(os.path.dirname(os.path.realpath(__file__)))
 print(path)
 filename=input("enter the name of the file it should be in same directory without extention")
-document = Document(path+'\\'+filename+'.docx')
-for table in document.tables:
-    sm=[]
-    for row in table.rows:
-        for cell in iter_unique_cells(row):
-            for paragraph in cell.paragraphs:
-                sm.append(paragraph.text)
-    data.append(sm)
+try:
+    document = Document(path+'\\'+filename+'.docx')
+    for table in document.tables:
+        sm=[]
+        for row in table.rows:
+            for cell in iter_unique_cells(row):
+                for paragraph in cell.paragraphs:
+                    sm.append(paragraph.text)
+        data.append(sm)
 
-fullText = []
-for para in document.paragraphs:
-        fullText.append(para.text)
-# print(fullText)
-for p in fullText:
-	if(p.isspace()):
-		fullText.remove(p)
-		#print('hello')
-copy=fullText.copy()
-for p in fullText:
-	if not(p.startswith('Q')):
-		copy.remove(p)
-		#print('hello')
-titil=[]
-for s in range(len(copy)):
-	titil.append((copy[s])[(1+(len(str(s+1)))):])
-# print(titil)
-    
-mc=[]
-ma=[]
-topic=input("enter topic name") 
-for k in data:
-    name=""
-    right=""
-    new_data=[]
-    if "MC" in k[1]:
-        name="MCQ";
-    else:
-        name="MCA"
-    new_data.append(name)
-    new_data.append(titil.pop())
-    new_data.append(k[0])
-    ind=[15,19,23,27]
-    ans="0"
-    for p in range(4):
-        new_data.append(k[ind[p]])
+    fullText = []
+    for para in document.paragraphs:
+            fullText.append(para.text)
+    # print(fullText)
+    for p in fullText:
+            if(p.isspace()):
+                    fullText.remove(p)
+                    #print('hello')
+    copy=fullText.copy()
+    for p in fullText:
+            if not(p.startswith('Q')):
+                    copy.remove(p)
+                    #print('hello')
+    titil=[]
+    for s in range(len(copy)):
+            titil.append((copy[s])[(1+(len(str(s+1)))):])
+    # print(titil)
         
-        if int(k[ind[p]+2]) >0:
-            if ans is "0":
-                ans = str(p+1)
-            else:
-                ans = ans+","+str(p+1)
-    new_data.append("")
-    new_data.append(ans)
-    new_data.append(topic)
-    if "MC" in k[1]:
-        mc.append(new_data)
-    else:
-        ma.append(new_data)
-    
-    
+    mc=[]
+    ma=[]
+    topic=input("enter topic name") 
+    for k in data:
+        name=""
+        right=""
+        new_data=[]
+        if "MC" in k[1]:
+            name="MCQ";
+        else:
+            name="MCA"
+        new_data.append(name)
+        if titil:
+            new_data.append(titil.pop())
+        else:
+            new_data.append('')
+        new_data.append(k[0])
+        ind=[15,19,23,27]
+        ans="0"
+        for p in range(4):
+            new_data.append(k[ind[p]])
+            
+            if int(k[ind[p]+2]) >0:
+                if ans == "0":
+                    ans = str(p+1)
+                else:
+                    ans = ans+","+str(p+1)
+        new_data.append("")
+        new_data.append(ans)
+        new_data.append(topic)
+        if "MC" in k[1]:
+            mc.append(new_data)
+        else:
+            ma.append(new_data)
+        
+        
 
 
 
 
-mc_df = pd.DataFrame(mc,columns =heads) 
-ma_df = pd.DataFrame(ma,columns =heads) 
+    mc_df = pd.DataFrame(mc,columns =heads) 
+    ma_df = pd.DataFrame(ma,columns =heads) 
 
-writer = pd.ExcelWriter(path+'\\'+filename+'.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(path+'\\'+filename+'.xlsx', engine='xlsxwriter')
 
-mc_df.to_excel(writer, sheet_name='Multiple choice question')
+    mc_df.to_excel(writer, sheet_name='Multiple choice question')
 
-ma_df.to_excel(writer, sheet_name='Multiple choice answer')
+    ma_df.to_excel(writer, sheet_name='Multiple choice answer')
 
 
 
-writer.save()
+    writer.save()
+    print("done ",len(data),' question found  - saved you ',2*len(data),' min')
+    time.sleep(2)
+except Exception as ex:
+    print('somthing went wrong'+ex)
